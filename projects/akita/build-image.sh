a81c949fd73e165ce07a4ce31675cd2844730453
Jono Finger <jfinger@plos.org>
2015-05-27 10:22:36 -0700

    Trying to get Akita to run again

--- projects/akita/build-image.sh
+++ projects/akita/build-image.sh
@@ -2,22 +2,38 @@
 
 SRC=../../../akita/
 
+PROJECTNAME=akita
+
+TMP_BUILD_CONTAINER=${PROJECTNAME}_temp_container
+
 DIR=$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )
 cd $DIR
 SRC=$DIR/$SRC
 
 docker build -t mailcatcher -f Dockerfile.mailcatcher .
 
-# this is a hack to allow the Dockerfile to exist in this subfolder
-
-echo SRC: $SRC
-
-cp Dockerfile $SRC
-time docker build -t akita:current $SRC
+cp Dockerfile $SRC      # this is a hack to allow the Dockerfile to exist in this subfolder
+cd $SRC
+time docker build -t $PROJECTNAME:current .
+cd $DIR
 rm $SRC/Dockerfile
 
-VERSION=$(docker run --rm akita:current cat /root/version.txt)
+# VERSION=$(docker run --rm $PROJECTNAME:current cat /root/version.txt)
 
-echo building AKITA version $VERSION
+VERSION=$(docker run --volume $DIR:/scripts \
+                     --volume $DIR/..:/shared \
+                     --name $TMP_BUILD_CONTAINER $PROJECTNAME:current \
+                     sh -c 'cp /shared/run-helpers.sh /root/; 
+                            cp /scripts/run.sh /root/;
+                            chmod 755 /root/run.sh;
+                            cat /root/version.txt')
 
-docker tag -f akita:current akita:$VERSION
+docker commit $TMP_BUILD_CONTAINER $PROJECTNAME:current
+
+# tag docker image with asset version number
+
+echo tagging container with version : $VERSION
+
+docker tag -f $PROJECTNAME:current $PROJECTNAME:$VERSION
+
+docker rm $TMP_BUILD_CONTAINER
