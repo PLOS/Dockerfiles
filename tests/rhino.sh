784cdf94276e8fe19cfeaeb168f27252cf1a30ca
Jono Finger <jfinger@plos.org>
2015-05-29 11:08:09 -0700

    Simplified tests and created one for rhino

---
+++ tests/rhino.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+
+SCRIPTDIR=$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )
+COMPOSE_FILE=rhino.yml
+
+source $SCRIPTDIR/test-helper.sh
+
+start_stack
+
+SVC_URL=$(get_service_ip rhino):8080
+
+wait_for_web_service $SVC_URL
+
+# rhino tests
+
+curl_test_ok $SVC_URL/articles
+
+# end rhino tests
+
+echo "TESTS PASSED"
+
+stop_stack
--- tests/test-helper.sh
+++ tests/test-helper.sh
@@ -1,11 +1,9 @@
 #!/bin/bash
 
-COMPOSE_DIR=../configurations
+COMPOSE_FILE=$SCRIPTDIR/../configurations/$COMPOSE_FILE
 
 function get_service_ip {
   CONTAINER_BASENAME=$1
-  COMPOSE_FILE=$SCRIPTDIR/$COMPOSE_DIR/$2
-
   CONTAINER=$(docker-compose -f $COMPOSE_FILE ps | grep $CONTAINER_BASENAME | awk 'END {print $1}')
 
   IP=$(docker inspect --format '{{ .NetworkSettings.IPAddress }}' $CONTAINER)
@@ -13,14 +11,41 @@ function get_service_ip {
   echo $IP
 }
 
-# function wait_for_service {
+function curl_test_ok {
 
-# }
+  URL=$1
+  echo "curling $URL"
 
-# function start_stack {
+  HTTP_CODE=$(curl -sL -w "%{http_code}\\n" $URL -o /dev/null)
+  if [[ "$HTTP_CODE" -ne "200" ]]; then
+    echo "TEST FAILED : url = $URL   status code = $HTTP_CODE"
+    exit 1
+  fi
+}
 
-# }
+function wait_for_web_service {
 
-# function stop_stack {
+  URL=$1
+  TEST_CMD="curl -I $URL"
+  TEST_RETURN_CODE=1
 
-# }
+  while [ $TEST_RETURN_CODE -ne 0 ] ; do
+    sleep 2;
+    $TEST_CMD
+    TEST_RETURN_CODE=$?
+    echo "Service ($URL) not ready ... waiting"
+
+    # TODO: exit after some time
+
+  done;
+
+  echo "Service ($URL) is up and ready for tests"
+}
+
+function start_stack {
+  docker-compose -f $COMPOSE_FILE up -d
+}
+
+function stop_stack {
+  docker-compose -f $COMPOSE_FILE kill
+}
