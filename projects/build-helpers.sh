319418becd9feb33bd2e93ad0272e8037dcf407e
saharaghajani <saghajani@plos.org>
2015-06-03 12:04:53 -0700

    Rename variables and add comments for readability

--- projects/build-helpers.sh
+++ projects/build-helpers.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
-MAVEN_CACHE=maven_cache
+MAVEN_LOCAL_REPO=maven_local_repo
 
-BUILD_CACHE=${PROJECTNAME}_build
+BUILD_RESULT_DIR=${PROJECTNAME}_build_result
 TMP_BUILD_CONTAINER=${PROJECTNAME}_temp_container
 
 function die () {
@@ -11,9 +11,12 @@ function die () {
 }
 
 function build_java_service_images() {
+    # checkout the project from git if it doesn't exist on the local machine
 
-	if [ ! -d $SRC ]; then
-	  echo "Source directory not found $SRC"
+	if [ ! -d $SRC ]
+	  then
+
+	  echo "Source directory not found $SRC; Cloning from github..."
 	  git --version > /dev/null || die "git is not installed"
 
 	  git clone git@github.com:PLOS/${PROJECTDIR}.git $SRC
@@ -21,51 +24,48 @@ function build_java_service_images() {
 	  if [ ! -d $SRC ]; then die "git clone failed"; fi
 	fi
 
-	# create build caches if they do not exist
+	# create build and maven repo volumes if they do not exist
+	docker inspect $BUILD_RESULT_DIR > /dev/null
+	[ $? -eq 1 ] && docker create -v /build --name $BUILD_RESULT_DIR $BASEIMAGE /bin/true
 
-	docker inspect $BUILD_CACHE > /dev/null
-	[ $? -eq 1 ] && docker create -v /build --name $BUILD_CACHE $BASEIMAGE /bin/true
-
-	docker inspect $MAVEN_CACHE > /dev/null
-	[ $? -eq 1 ] && docker create -v /root/.m2 --name $MAVEN_CACHE $BASEIMAGE /bin/true
+	docker inspect $MAVEN_LOCAL_REPO > /dev/null
+	[ $? -eq 1 ] && docker create -v /root/.m2 --name $MAVEN_LOCAL_REPO $BASEIMAGE /bin/true
 
 	# build API war
-
-	echo Building java assets
+	echo "Building and Loading Java Assets on Base Image (maven)..."
 
 	docker run -it \
-	   --volumes-from $MAVEN_CACHE \
-	   --volumes-from $BUILD_CACHE \
+	   --volumes-from $MAVEN_LOCAL_REPO \
+	   --volumes-from $BUILD_RESULT_DIR \
 	   --volume $SRC:/src \
 	   --volume $DIR:/scripts \
 	   $BASEIMAGE bash /scripts/compile.sh
 
-	echo Building base image
+	echo "Building base image ..."
 
 	docker build -t $PROJECTNAME:current .
 
 	# copy assets and scripts into image
 
-	echo Copying java assets into image
+	echo "Copying Java Assets into Tomcat Image..."
 
-	VERSION=$(docker run --volume $DIR:/scripts \
-											 --volume $DIR/..:/shared \
-											 --volumes-from $BUILD_CACHE \
-											 --name $TMP_BUILD_CONTAINER $PROJECTNAME:current \
-											 sh -c 'cp /build/* /root; \
-											 				cp /shared/run-helpers.sh /root/; 
-											 				cp /scripts/run.sh /root/;
-											 				chmod 755 /root/run.sh;
-											 				cat /root/version.txt')
+	VERSION=$(docker run \
+	    --volume $DIR:/scripts \
+		--volume $DIR/..:/shared \
+		--volumes-from $BUILD_RESULT_DIR \
+		--name $TMP_BUILD_CONTAINER $PROJECTNAME:current \
+		sh -c 'cp -r /build/* /root; \
+		cp /shared/run-helpers.sh /root/;
+		cp /scripts/run.sh /root/;
+		chmod 755 /root/run.sh;
+		cat /root/version.txt')
 
 	docker commit $TMP_BUILD_CONTAINER $PROJECTNAME:current
 
 	# tag docker image with asset version number
-
-	echo tagging container with version : $VERSION
+	echo "tagging container with version : $VERSION"
 
 	docker tag -f $PROJECTNAME:current $PROJECTNAME:$VERSION
 
 	docker rm $TMP_BUILD_CONTAINER
-
 }
