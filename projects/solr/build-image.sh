d799ae1558c24edb10f6535fb1610703b21d54f7
saharaghajani <saghajani@plos.org>
2015-06-03 14:21:35 -0700

    BAU-3152 refactored based on changes in master

--- projects/solr/build-image.sh
+++ projects/solr/build-image.sh
@@ -1,16 +1,5 @@
 #!/bin/bash
 
-PROJECTNAME=solr
-PROJECTDIR=solr
+$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )/../build-helpers.sh \
+  build_java_service_images maven:3.3-jdk-7 solr solr
 
-BASEIMAGE=maven:3.3-jdk-8
-
-SRC=../../../${PROJECTDIR}/
-
-DIR=$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )
-SRC=$DIR/$SRC
-
-cd $DIR
-source ../build-helpers.sh
-
-build_java_service_images
