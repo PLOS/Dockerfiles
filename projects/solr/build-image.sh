09b02f0399cc6e58988a160b509215990deebbb3
saharaghajani <saghajani@plos.org>
2015-06-04 09:59:45 -0700

    BAU-3152 used $CATALINA_HOME insated of  /usr/local/tomcat in paths

--- projects/solr/build-image.sh
+++ projects/solr/build-image.sh
@@ -2,4 +2,3 @@
 
 $( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )/../build-helpers.sh \
   build_java_service_images maven:3.3-jdk-7 solr solr
-
--- projects/solr/run.sh
+++ projects/solr/run.sh
@@ -2,21 +2,21 @@
 
 BUILD_DIR=/root
 
-cp /usr/local/tomcat/conf/* /etc/ambra
-rm -rf /usr/local/tomcat/conf
-ln -s /etc/ambra /usr/local/tomcat/conf
-cp $BUILD_DIR/log4j.xml /usr/local/tomcat/lib
+cp $CATALINA_HOME/conf/* /etc/ambra
+rm -rf $CATALINA_HOME/conf
+ln -s /etc/ambra $CATALINA_HOME/conf
+cp $BUILD_DIR/log4j.xml $CATALINA_HOME/lib
 
 SVC_WAR=solr.war
 source $BUILD_DIR/run-helpers.sh
 
 unzip $BUILD_DIR/solr.war -d  $BUILD_DIR/temp/
-cp -r $BUILD_DIR/temp/collection1 /usr/local/tomcat/solr/home/
+cp -r $BUILD_DIR/temp/collection1 $CATALINA_HOME/solr/home/
 rm -rf  $BUILD_DIR/temp/
 
 setup_war_in_tomcat
 
-export JAVA_OPTS="$JAVA_OPTS -Dsolr.solr.home=/usr/local/tomcat/solr/home"
-export JAVA_OPTS="$JAVA_OPTS -Dsolr.data.dir=/usr/local/tomcat/solr/home/data"
+export JAVA_OPTS="$JAVA_OPTS -Dsolr.solr.home=$CATALINA_HOME/solr/home"
+export JAVA_OPTS="$JAVA_OPTS -Dsolr.data.dir=$CATALINA_HOME/solr/home/data"
 
 start_tomcat
