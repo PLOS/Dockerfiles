6f2f1cd892923b02e6600749c822daf2f12c96af
saharaghajani <saghajani@plos.org>
2015-06-03 17:07:32 -0700

    BAU-3152 modify solr specific paths
    create new directories in the Dockerfile
    mount the solr index volume in the created directory
    change paths and orders of commands in run.sh

--- projects/solr/run.sh
+++ projects/solr/run.sh
@@ -10,16 +10,13 @@ cp $BUILD_DIR/log4j.xml /usr/local/tomcat/lib
 SVC_WAR=solr.war
 source $BUILD_DIR/run-helpers.sh
 
-setup_war_in_tomcat
-
-mkdir /usr/local/tomcat/webapps/home
-mkdir /usr/local/tomcat/webapps/home/data
-
 unzip $BUILD_DIR/solr.war -d  $BUILD_DIR/temp/
-cp -r $BUILD_DIR/temp/collection1 /usr/local/tomcat/webapps/home/
+cp -r $BUILD_DIR/temp/collection1 /usr/local/tomcat/solr/home/
 rm -rf  $BUILD_DIR/temp/
 
-export JAVA_OPTS="$JAVA_OPTS -Dsolr.solr.home=/usr/local/tomcat/webapps/home"
-export JAVA_OPTS="$JAVA_OPTS -Dsolr.data.dir=/usr/local/tomcat/webapps/home/data"
+setup_war_in_tomcat
+
+export JAVA_OPTS="$JAVA_OPTS -Dsolr.solr.home=/usr/local/tomcat/solr/home"
+export JAVA_OPTS="$JAVA_OPTS -Dsolr.data.dir=/usr/local/tomcat/solr/home/data"
 
 start_tomcat
