8e48997fd4375eb3ec7f485f22724f0c5c674fb4
saharaghajani <saghajani@plos.org>
2015-06-17 17:25:18 -0700

    BAU-3152 mount only ingest folder
    Create proper directories and copy zip from from mounted volume to the created
    directory.

--- projects/rhino/run.sh
+++ projects/rhino/run.sh
@@ -11,8 +11,11 @@ wait_until_db_service_up
 # TODO: use a more up to date SQL schema dump
 if ! check_db_exists; then
   echo "CREATE SCHEMA $MYSQL_DATABASE" | $MYSQL_ROOT
-  $MYSQL_ROOT $MYSQL_DATABASE < ${BUILD_DIR}/ambra_schema.sql
+  $MYSQL_ROOT $MYSQL_DATABASE < ${BUILD_DIR}/ambra_schema_1005.sql
   $MYSQL_ROOT $MYSQL_DATABASE < ${BUILD_DIR}/ambra_data.sql
+ # $MYSQL_ROOT $MYSQL_DATABASE < ${BUILD_DIR}/volume.sql
+ # $MYSQL_ROOT $MYSQL_DATABASE < ${BUILD_DIR}/articleList.sql
+  $MYSQL_ROOT $MYSQL_DATABASE < ${BUILD_DIR}/issue.sql
 fi
 
 set_db_grants
@@ -22,6 +25,7 @@ rm -rf /usr/local/tomcat/conf
 ln -s /etc/ambra /usr/local/tomcat/conf
 cp ${BUILD_DIR}/*.xml /etc/ambra
 cp ${BUILD_DIR}/rhino.yaml /etc/ambra
+cp -r /root/ingest/* /root/datastores/ingest/
 
 setup_war_in_tomcat
 
