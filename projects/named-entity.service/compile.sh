a8e94b337f8354e2edc53060d0f9ef178e412444
Matthew Bowen <mbowen@plos.org>
2015-10-13 09:50:48 -0700

    build and install maven docker plugin

--- projects/named-entity.service/compile.sh
+++ projects/named-entity.service/compile.sh
@@ -4,6 +4,10 @@ source /shared/compile-helpers.sh
 
 java_compile_prepare
 
+cd config/docker-maven-plugin
+mvn clean install
+cd ../..
+
 mvn -Dmaven.exec.skip=true -Dmaven.test.skip=true install
 cp target/*.?ar $BUILDDIR
 cp src/main/resources/ned-*.mysql.sql $BUILDDIR
