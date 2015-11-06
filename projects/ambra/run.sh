8bdaba79821184fcf0c4ce7e79903e72b0b1f38b
Jono Finger <jfinger@plos.org>
2015-11-06 10:24:48 -0800

    Added updated schema ambra schema files to support rhino running without ambra.

--- projects/ambra/run.sh
+++ projects/ambra/run.sh
@@ -9,8 +9,12 @@ wait_until_db_service_up
 if ! check_db_exists; then
   echo "CREATE SCHEMA $MYSQL_DATABASE" | $MYSQL_ROOT
   $MYSQL_ROOT $MYSQL_DATABASE < ${BUILD_DIR}/ambra_schema_1005.sql
+  $MYSQL_ROOT $MYSQL_DATABASE < ${BUILD_DIR}/migrate_ambra_1006.sql
+  $MYSQL_ROOT $MYSQL_DATABASE < ${BUILD_DIR}/migrate_ambra_1007.sql
   $MYSQL_ROOT $MYSQL_DATABASE < ${BUILD_DIR}/ambra_data.sql
   $MYSQL_ROOT $MYSQL_DATABASE < ${BUILD_DIR}/issue.sql
+else
+  echo "Database already exists $MYSQL_DATABASE"
 fi
 
 set_db_grants
