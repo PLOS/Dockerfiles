40fbd184ac416d5f516e8164455549e845b20e87
Jono Finger <jfinger@plos.org>
2015-12-13 21:04:17 -0800

    Added ssl support to NED in tomcat

--- tests/akita_nedapi_nedcas.sh
+++ tests/akita_nedapi_nedcas.sh
@@ -20,7 +20,7 @@ wait_for_web_service $CAS "CAS"
 # begin tests
 
 curl_test_ok $AKITA/registration/new $AKITA_NAME
-curl_test_ok $DOCKER_HOST:8081/service/config "NED API"
+curl_test_ok $DOCKER_HOST:8081/v0/service/config "NED API"
 curl_test_ok $CAS/cas/login "CAS"
 curl_test_ok $DOCKER_HOST:1080 "Mailcatcher"
 
