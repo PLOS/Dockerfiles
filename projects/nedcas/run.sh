e2be6895128c26df24ccbb86e29d02c5b3f119f0
Matthew Bowen <mbowen@plos.org>
2016-10-14 09:17:03 -0700

    PLT-363: update dockerfiles for clustered cas (hazelcast) *** NOT COMPLETE ***

--- projects/nedcas/run.sh
+++ projects/nedcas/run.sh
@@ -6,7 +6,8 @@ SVC_WAR=ned-cas-*.war
 
 source $BUILD_DIR/run-helpers.sh
 
-process_template ${CATALINA_HOME}/conf/context.xml
+#process_template ${CATALINA_HOME}/conf/context.xml
+process_template /etc/cas/cas.properties
 
 wait_for_web_service $NED_SERVICE/service/config "NED"
 
