3e93025edd47810c11ff459b42f9f2c5b2351eef
Jono Finger <jfinger@plos.org>
2016-10-27 16:25:54 -0700

    Still trying to get proxy to work right with https

--- tests/akita.sh
+++ tests/akita.sh
@@ -3,8 +3,7 @@
 source /dockerfiles/tests/test-helpers.sh
 
 wait_and_curl nedapi:8080 /v1/service/config "NED API"
-# wait_and_curl https://$DOCKER_HOST:8444 "/v1/service/config" "NED API ssl"
-wait_and_curl https://nedcas-lb:443 /cas/login "CAS"
-wait_and_curl https://akita-lb /registration/new "Akita"
+wait_and_curl https://nedcas-ssl:443 /cas/login "CAS"
+wait_and_curl https://akita-ssl /registration/new "Akita"
 wait_and_curl mailcatcher:1080 / "Mailcatcher"
-test_up https://akita-lb/flanders/v1/types/countries "Rails_to_NED"
+test_up https://akita-ssl/flanders/v1/types/countries "Rails_to_NED"
