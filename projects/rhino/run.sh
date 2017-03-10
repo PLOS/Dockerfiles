26024fde3d8682013eea742dba24af467696a5ca
Jono Finger <jfinger@plos.org>
2017-03-09 16:13:37 -0800

    Revering back to the old config generation of processing a local rhino.yaml file instead of using the python script

--- projects/rhino/run.sh
+++ projects/rhino/run.sh
@@ -5,7 +5,6 @@ source $HOME/run-helpers.sh
 require_mysql_envs
 # require_envs REPO_LOCATION CORPUS_BUCKET \
 #   EDITORIAL_BUCKET QUEUE_LOCATION USER_API_SERVER USER_API_USER USER_API_AUTH
-
 require_envs REPO_LOCATION CORPUS_BUCKET
 
 wait_until_db_service_up
@@ -36,17 +35,19 @@ curl -X POST $REPO_LOCATION/buckets --data name=corpus
 process_env_template $AMBRA_CONF/context.xml
 # process_env_template $AMBRA_CONF/ambra.xml      # TODO: figure out if ambra.xml is needed still
 
+# TODO: fetch rhino.yaml from salt (https://github.com/PLOS-Formulas/rhino-formula/blob/dev/rhino/conf/opt/plos/rhino/conf/rhino.yaml)? or better yet from the rhino repo if it gets added to the deb
+process_env_template $AMBRA_CONF/rhino.yaml
 
-$HOME/build_config_rhino.py                \
-    --repo_location     $REPO_LOCATION     \
-    --corpus_bucket     $CORPUS_BUCKET     \
-    --editorial_bucket  $EDITORIAL_BUCKET  \
-    --user_api_server   $USER_API_SERVER   \
-    --user_api_auth     $USER_API_AUTH     \
-    --queue_location    $QUEUE_LOCATION > $AMBRA_CONF/rhino.yaml || die "Config error"
-
-# HACK: inject NED username. needed because username is hardcoded in config
-sed -i "s/authorizationAppName:\s*dipro.*/authorizationAppName: $USER_API_USER/" $AMBRA_CONF/rhino.yaml
+# $HOME/build_config_rhino.py                \
+#     --repo_location     $REPO_LOCATION     \
+#     --corpus_bucket     $CORPUS_BUCKET     \
+#     --editorial_bucket  $EDITORIAL_BUCKET  \
+#     --user_api_server   $USER_API_SERVER   \
+#     --user_api_auth     $USER_API_AUTH     \
+#     --queue_location    $QUEUE_LOCATION > $AMBRA_CONF/rhino.yaml || die "Config error"
+#
+# # HACK: inject NED username. needed because username is hardcoded in config
+# sed -i "s/authorizationAppName:\s*dipro.*/authorizationAppName: $USER_API_USER/" $AMBRA_CONF/rhino.yaml
 
 export JAVA_OPTS="-Drhino.configDir=$AMBRA_CONF"
 start_tomcat
