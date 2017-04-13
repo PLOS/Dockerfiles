dec5b1eefdfd522d3a7afece2eac1d3233abd858
Jono Finger <jfinger@plos.org>
2017-04-13 12:18:45 -0700

    Got wombat working in example mode

--- projects/wombat/build-image.sh
+++ projects/wombat/build-image.sh
@@ -2,12 +2,12 @@
 
 source $( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )/../../envoy-config.sh
 
-# HACK: copy python scripts over from ploth-themes
-check_local_src plos-themes
-
-cp $(get_local_src_dir plos-themes)/{requirements.txt,config/build_config_utils.py,config/build_config_wombat.py} "$( dirname "${BASH_SOURCE[0]}" )" || die "cant find config generator"
+# # HACK: copy python scripts over from ploth-themes
+# check_local_src plos-themes
+#
+# cp $(get_local_src_dir plos-themes)/{requirements.txt,config/build_config_utils.py,config/build_config_wombat.py} "$( dirname "${BASH_SOURCE[0]}" )" || die "cant find config generator"
 
 build_image_maven wombat wombat
 
-# hack cleanup
-rm "$( dirname "${BASH_SOURCE[0]}" )"/{requirements.txt,build_config_utils.py,build_config_wombat.py}
+# # hack cleanup
+# rm "$( dirname "${BASH_SOURCE[0]}" )"/{requirements.txt,build_config_utils.py,build_config_wombat.py}
