c5879040c9d66f4f7d50aaf178c7c952a414f832
Jono <jono@foodnotblogs.com>
2017-05-05 22:04:01 -0700

    Ember builder fixed

--- projects/akita/compile.sh
+++ projects/akita/compile.sh
@@ -4,19 +4,16 @@ source /envoy/compile-helpers.sh
 
 compile_prepare
 
-mkdir rails
-# cp -r `ls -A | grep -Ev "frontend"` $BUILDDIR/rails
+mkdir $BUILDDIR/rails
+mv /root/src/frontend /tmp/frontend
 mv /root/src/* $BUILDDIR/rails
-rm -rf $BUILDDIR/frontend
 
-cd frontend
+cd /tmp/frontend
 
 # only run the setup if the dependencies are missing
 ls node_modules || bin/setup
 ls dist || ./node_modules/.bin/ember build --environment=production
 
 cp -r dist $BUILDDIR/ember
-
-# ls -l ..
 
 echo `node -e "var fs = require('fs');console.log(JSON.parse(fs.readFileSync('package.json', 'utf8'))['version']);"` > $BUILDDIR/version.txt
