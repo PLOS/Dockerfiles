166ad162b16b0aa116f3154edb3e856c4139bb78
Jono <jono@foodnotblogs.com>
2017-05-08 07:11:38 -0700

    Fixed Akita run helper. Now it works to the backend and is 268mb

--- tests/akita.sh
+++ tests/akita.sh
@@ -6,6 +6,6 @@ wait_and_curl nedapi:8080 /v1/service/config "NED API"
 wait_and_curl nedcas:8080 /cas/login "CAS"
 wait_and_curl akita /registration/new "Akita"
 wait_and_curl mailcatcher:1080 / "Mailcatcher"
-# test_up akita/flanders/v1/types/countries "Rails_to_NED"  # figure out why nginx is giving trouble here
-curl akita | grep 'akita' || (echo "akita site no up" && exit 1)
-# https://community.plos.org/flanders/status/more
+test_up akita/flanders/v1/types/countries "Rails_to_NED" # | jq . | wc -l
+curl akita | grep 'akita' || (echo "akita frontend site not up" && exit 1)
+# akita/flanders/status/more  # breaks when ringgold db is not created?
