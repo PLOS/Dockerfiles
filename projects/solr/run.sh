d95bbaf14a5cde8c85ba2168e278035eda47ddc6
Jono Finger <jfinger@plos.org>
2017-05-12 11:18:29 -0700

    Fixing run helper for starting tomcat on another port

--- projects/solr/run.sh
+++ projects/solr/run.sh
@@ -6,3 +6,4 @@ export JAVA_OPTS="$JAVA_OPTS -Dsolr.solr.home=$CATALINA_HOME/solr/home"
 export JAVA_OPTS="$JAVA_OPTS -Dsolr.data.dir=$CATALINA_HOME/solr/home/data"
 
 start_tomcat
+# start_tomcat.sh
