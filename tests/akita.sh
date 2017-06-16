5248f3003a038f2826bb1363f754476256eae5e4
Jono <jono@foodnotblogs.com>
2017-06-15 17:32:37 -0700

    Update akita test

--- tests/akita.sh
+++ tests/akita.sh
@@ -7,5 +7,6 @@ wait_and_curl nedcas:8080 /cas/login "CAS"
 wait_and_curl akita /registration/new "Akita"
 wait_and_curl mailcatcher:1080 / "Mailcatcher"
 test_up akita/flanders/v1/types/countries "Rails_to_NED" # | jq . | wc -l
+test_up akita/flanders/status/more "Rails_to_NED_status" # | jq . | wc -l
 curl akita | grep 'akita' || (echo "akita frontend site not up" && exit 1)
 # akita/flanders/status/more  # breaks when ringgold db is not created?
