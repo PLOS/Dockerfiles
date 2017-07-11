63c4d5d6734a0029666716b05dada6442d196891
Jono Finger <jfinger@plos.org>
2017-07-11 05:26:10 -0700

    Fixing mailcatcher port in akita test

--- tests/akita.sh
+++ tests/akita.sh
@@ -5,7 +5,7 @@ source /envoy/test-helpers.sh
 wait_and_curl nedapi:8080 /v1/service/config "NED API"
 wait_and_curl nedcas:8080 /cas/login "CAS"
 wait_and_curl akita /registration/new "Akita"
-wait_and_curl mailcatcher:1080 / "Mailcatcher"
+wait_and_curl mailcatcher:80 / "Mailcatcher"
 test_up akita/flanders/v1/types/countries "Rails_to_NED" # | jq . | wc -l
 test_up akita/flanders/status/more "Rails_to_NED_status" # | jq . | wc -l
 curl akita | grep 'akita' || (echo "akita frontend site not up" && exit 1)
