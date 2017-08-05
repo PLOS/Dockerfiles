e16db359dee4e6de312f0f9438fc83ac7702fdfb
Jono Finger <jfinger@plos.org>
2017-08-05 15:28:29 -0700

    More cmd.sh cleanup

--- projects/wombat/compile.sh
+++ projects/wombat/compile.sh
@@ -4,6 +4,8 @@ source /envoy/compile-helpers.sh
 
 compile_prepare
 
+# TODO: perhaps we can build this again using alpine and combine it with an alpine node via multistage build so we dont have to download the large debian JDK image. this mean mvn here would have to skip the node phase somehow
+
 mvn -Dmaven.test.skip=true clean package
 cp target/*.war $BUILDDIR
 
