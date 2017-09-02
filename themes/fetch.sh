51e0154677e5969d25a4dcf8c8283bdfe9429671
Jono <jono@foodnotblogs.com>
2017-09-01 22:19:18 -0700

    Clean up theme fetch script

--- themes/fetch.sh
+++ themes/fetch.sh
@@ -1,35 +1,51 @@
 #!/usr/bin/env bash
 
+set -x
 set -e
 
 cd $( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )
 
-# to clean out and start fresh: rm -rf minimal plos-*
-
 # TODO: handle update case better?
 
-echo Fetching minimal themes...
-
-if [[ -d "minimal" ]]; then
-  echo minimal directory already exists. Delete before fetching if you want.
-else
-  wget -O - https://plos.github.io/ambraproject/example/themes.tar.gz | tar xz && mv themes minimal
+if [[ "$1" == "clean" ]]; then
+  rm -rf minimal plos-example-themes plos-themes
+  exit 0
 fi
 
-echo Fetching example plos themes...
+if [ "$#" -eq 0 ] || [[ "$1" == "minimal" ]]; then
 
-if [[ -d "plos-example-themes" ]]; then
-  echo plos-example-themes directory already exists. Delete before fetching if you want.
-else
-  wget -O - http://downloads.ambraproject.org/releases/plos-example-themes.tar.gz | tar xz
+  echo Fetching minimal themes...
+
+  if [[ -d "minimal" ]]; then
+    echo minimal directory already exists. Delete before fetching if you want.
+  else
+    curl https://plos.github.io/ambraproject/example/themes.tar.gz | tar xz && mv themes minimal
+  fi
+
 fi
 
-echo Fetching production plos themes, if you have permission...
+if [ "$#" -eq 0 ] || [[ "$1" == "plos-example-themes" ]]; then
 
-# TODO: check git permissions before the pull or clone
+  echo Fetching example plos themes...
 
-if [[ -d "plos-themes" ]]; then
-  echo plos-themes directory already exists. Delete before fetching if you want.
-else
-  git clone git@github.com:PLOS/plos-themes
+  if [[ -d "plos-example-themes" ]]; then
+    echo plos-example-themes directory already exists. Delete before fetching if you want.
+  else
+    curl http://downloads.ambraproject.org/releases/plos-example-themes.tar.gz | tar xz
+  fi
+
+fi
+
+if [ "$#" -eq 0 ] || [[ "$1" == "plos-themes" ]]; then
+
+  echo Fetching production plos themes, if you have permission...
+
+  # TODO: check git permissions before the pull or clone
+
+  if [[ -d "plos-themes" ]]; then
+    echo plos-themes directory already exists. Delete before fetching if you want.
+  else
+    git clone git@github.com:PLOS/plos-themes
+  fi
+
 fi
