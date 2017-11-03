ee91dba811a05b69586919ec8181f0e7003bd193
Jono Finger <jfinger@plos.org>
2017-11-03 15:31:38 -0700

    Cleaning up used containers in ploscrawl test

--- tests/ploscrawl.py
+++ tests/ploscrawl.py
@@ -15,7 +15,9 @@ class Test():
 
   def test_scraper_list(self, stack):
 
-    scrapernames = str(self.d.containers.run('plos/ploscrawl:dev', 'scrapy list', detach=False, stderr=True))  #, auto_remove=True
+    scrapernames = str(self.d.containers.run('plos/ploscrawl:dev', 'scrapy list', detach=False, stderr=True, name='scrapy_list'))  #, auto_remove=True
+
+    self.d.containers.get('scrapy_list').remove()
 
     assert 'blogs' in scrapernames
     assert 'journalarticles' in scrapernames
@@ -25,11 +27,8 @@ class Test():
 
     assert requests.get(solr + "/solr/site_search/select?fl=*&indent=on&q=*:*&wt=json").json()['response']['numFound'] == 0, 'solr should be empty'
 
-    self.d.containers.run('plos/ploscrawl:dev', 'scrapy crawl journalarticles -a urls="http://journals.plos.org/plosone/article?id=10.1371/journal.pone.0028001,http://journals.plos.org/plosone/article?id=10.1371/journal.pone.0027963" -a solr_url='+ solr +'/solr/site_search', network='configurations_default', detach=False, stderr=True)
+    self.d.containers.run('plos/ploscrawl:dev', 'scrapy crawl journalarticles -a urls="http://journals.plos.org/plosone/article?id=10.1371/journal.pone.0028001,http://journals.plos.org/plosone/article?id=10.1371/journal.pone.0027963" -a solr_url='+ solr +'/solr/site_search', network='configurations_default', detach=False, stderr=True, name='scrapy_crawl')
 
-    # TODO: figure out how to clean up containers after they runs
-    # , name='runcrawl'
-    # auto_remove=True fails for some reason
-    # self.d.containers.prune({name:'runcrawl'})
+    self.d.containers.get('scrapy_crawl').remove()
 
     assert requests.get(solr + "/solr/site_search/select?fl=*&indent=on&q=*:*&wt=json").json()['response']['numFound'] == 2, 'solr should now have 2 articles in it'
